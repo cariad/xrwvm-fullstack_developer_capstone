3dad29f773dea468135dac45e1624bfe20c94706
Cariad Eccleston <cariad@cariad.earth>
2024-10-06 05:31:00 -0400

    Implement review routes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
+import Dealer from "./components/Dealers/Dealer"
+import Dealers from './components/Dealers/Dealers';
 import LoginPanel from "./components/Login/Login"
+import PostReview from "./components/Dealers/PostReview"
 import RegistrationPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/dealers" element={<Dealers/>} />
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
       <Route path="/register" element={<RegistrationPanel />} />
     </Routes>
   );
