ac31e1ef0b25e0fe266eacaea35e22d289c72c26
Cariad Eccleston <cariad@cariad.earth>
2024-10-06 10:46:30 +0100

    Fix more lint warnings

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,21 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url',
+    "backend_url",
     default="http://localhost:3030",
 )
 
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
+    "sentiment_analyzer_url",
     default="http://localhost:5050/",
 )
+
 
 def get_request(endpoint, **kwargs):
     params = ""
 
-    if(kwargs):
+    if kwargs:
         for key, value in kwargs.items():
-            params=params + key + "=" + value + "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
@@ -30,8 +31,9 @@ def get_request(endpoint, **kwargs):
     except:
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
 
     try:
         response = requests.get(request_url)
@@ -40,6 +42,7 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
 
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
