fe34991bd4ae96374d7ee46f3affa731b7602d83
Cariad Eccleston <cariad@cariad.earth>
2024-10-06 10:41:31 +0100

    Fix some lint warnings

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,19 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url',
+    default="http://localhost:3030",
+)
+
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="http://localhost:5050/",
+)
 
 def get_request(endpoint, **kwargs):
     params = ""
 
     if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+        for key, value in kwargs.items():
+            params=params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     try:
         response = requests.get(request_url)
@@ -27,7 +31,7 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/"+text
 
     try:
         response = requests.get(request_url)
@@ -38,10 +42,10 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
 
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
 
